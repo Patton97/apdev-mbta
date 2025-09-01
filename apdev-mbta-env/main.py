fbe1837624388713063f912a171dfb52e8b4e7a9
AndrewPattonDev <atpatton97@gmail.com>
2025-09-01 20:48:24 +0100

    begun using pygame for pre-LED software rendering

--- apdev-mbta-env/main.py
+++ apdev-mbta-env/main.py
@@ -3,15 +3,47 @@ import json
 
 import apdev_mbta_api_wrapper
 
-params = apdev_mbta_api_wrapper.GetStopsParams()
-params.sort = 'name'
-params.routeTypes = [0,1]
+def runAPITest():
+    params = apdev_mbta_api_wrapper.GetStopsParams()
+    params.sort = 'name'
+    params.routeTypes = [0,1]
 
-with requests.Session() as s:
-    response = apdev_mbta_api_wrapper.getStops(s, params)
+    with requests.Session() as s:
+        response = apdev_mbta_api_wrapper.getStops(s, params)
 
-resultsJsonObj = json.loads(response.content)
-results = apdev_mbta_api_wrapper.parseResultsJson(resultsJsonObj)
+    resultsJsonObj = json.loads(response.content)
+    results = apdev_mbta_api_wrapper.parseResultsJson(resultsJsonObj)
 
-for result in results:
-    print(result.name)
+    for result in results:
+        print(result.name)
+
+def runPygameTest():
+    import pygame
+
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill("black")
+
+        pygame.draw.circle(screen, "green", pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2), 4)
+
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        clock.tick(60)  # limits FPS to 60
+
+    pygame.quit()
+
+runPygameTest()
