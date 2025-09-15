8e3f754c2f11af1e174b1a027eb60745ebe3ef08
AndrewPattonDev <atpatton97@gmail.com>
2025-09-15 16:59:03 +0100

    Adding missing enum from previous commit

--- apdev-mbta-env/apddev_mbta_api_wrapper/vehicles.py
+++ apdev-mbta-env/apddev_mbta_api_wrapper/vehicles.py
@@ -1,11 +1,18 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from enum import Enum
 
 import json
 import requests
 
 from . import utils
-from .enums import VehicleStopStatus
+
+# https://github.com/google/transit/blob/master/gtfs-realtime/spec/en/reference.md#enum-vehiclestopstatus
+class VehicleStopStatus(Enum):
+    UNKNOWN       = -1
+    INCOMING_AT   = 0 # The vehicle is just about to arrive at the stop (on a stop display, the vehicle symbol typically flashes).
+    STOPPED_AT    = 1 # The vehicle is standing at the stop.
+    IN_TRANSIT_TO = 2 # The vehicle has departed the previous stop and is in transit.
 
 @dataclass(frozen=True)
 class ImmutableVehicle(object):
@@ -41,7 +48,7 @@ def __parseGetVehiclesResponseContent(jsonObj:dict) -> list[ImmutableVehicle]:
         if item["type"] != "vehicle":
             continue
 
-        if item['attributes']['current_status'] != VehicleStopStatus.STOPPED_AT:
+        if item['attributes']['current_status'] != VehicleStopStatus.STOPPED_AT.name:
             continue
         vehicle = ImmutableVehicle(
             id=item["id"],
